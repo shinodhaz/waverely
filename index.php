964e98b597588cf4ad544c8e8a3ce83c6a41a46a
shinodhaz <shinodhaz@gmail.com>
2021-07-02 22:04:14 +0700

    header and footer - desktop only

--- index.php
+++ index.php
@@ -1,12 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/main.css?v=<?php echo time() ?>">
+    <title>Waverely</title>
 </head>
+
 <body>
-    test
+    <header>
+        <div class="header-container">
+            <div class="logo">
+                <a href="#">
+                    <img src="assets/images/logo.svg" alt="Waverley Logo">
+                </a>
+            </div>
+
+            <div class="header-menu">
+                <ul id="menu-header-menu" class="menu inline">
+                    <li>
+                        <a href="#">AT A GLANCE</a>
+                    </li>
+                    <li>
+                        <a href="#">WATCH VIDEO</a>
+                    </li>
+                    <li>
+                        <a href="#">REGISTER</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div class="main-content">
+        <div class="top-banner">
+            <img src="assets/images/home-banner.jpg" alt="">
+        </div>
+    </div>
+    <footer>
+        <div class="footer-container">
+            <div class="logo">
+                <img src="assets/images/logo.svg" alt="Waverley Logo">
+            </div>
+            <div class="right-container">
+                <img src="assets/images/savills.svg" alt="savills">
+            </div>
+        </div>
+    </footer>
 </body>
+
 </html>
